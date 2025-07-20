8c24fcbaa7e16584e5201531b7d8d8759e6fa940
Agustin Portantier <agustinportantier17@hotmail.com>
2025-07-20 00:18:59 -0300

    Este es el Primer Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//El proyecto es una web para ingresar lista de libros leidos, deseados y pendientes.
+// Minimo 1 condicional (if, else)
+// Minimo 1 ciclo (while, for)
+// Minimo 3 funciones con parametros
+// Prompts, alerts y console
+
+// 3 Arrays. uno para libros leidos, otro para libros deseados y otro para libros pendientes.
+const librosLeidos = [];
+const librosDeseados = [];
+const librosPendientes = [];
+
+// Definir las funciones: 1 para agregar libro, otro para ver todos los libros, otro para borrar libro. La idea es navegar, borrar un libro deseado para agregarlo a pendiente y luego borrar un libro pendiente cuando fue leido.
+AAA;
